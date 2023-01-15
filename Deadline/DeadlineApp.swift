f69e5f3b63dfcecd3bed138fd9e1812f74da64ba
Matthew Seah <matscottseah@gmail.com>
2023-01-14 21:05:06 -0800

    Initial Commit

---
+++ Deadline/DeadlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeadlineApp.swift
+//  Deadline
+//
+//  Created by Matthew Seah on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DeadlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
